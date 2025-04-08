a35faed46e6755f53d430093957b8e53340bcca2
Bradley Golden <golden.bradley@gmail.com>
2025-04-07 19:56:52 -0500

    move version

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Alchemind.OpenAI.MixProject do
   use Mix.Project
 
+  @version "0.1.0-rc1"
+
   def project do
     [
       app: :alchemind_openai,
-      version: "0.1.0-rc1",
+      version: @version,
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
